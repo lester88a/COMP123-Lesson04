c9dc7d170269c2f1ecc612f7dd23d2b277766896
lester88a <lester88a@gmail.com>
2015-01-27 13:39:25 -0500

    New Hero

---
+++ COMP123-Lesson04-Part2/Hero.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Lesson04_Part2
+{
+    class Hero
+    {
+        //creat private properties+++++++++++++++++++++++++++++++++
+        private bool bravery = true;
+        private int health;
+        private int smarts;
+        private string gender;
+
+
+        //constructor++++++++++++++++++++++++++++++++++++++++++++
+        public Hero(int health, int smarts, string gender)
+        {
+            this.health = health;
+            this.smarts = smarts;
+            this.gender = gender;
+        }
+
+        //public methods
+        public void doesGoodThings()
+        {
+            Console.WriteLine("Doin' good thins....YEAH!");
+        }
+    }
+}
